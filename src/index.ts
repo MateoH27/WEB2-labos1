259a01a1b5bd158374ada93b1f848f6a243bc1c8
MateoH27 <115390699+MateoH27@users.noreply.github.com>
2023-10-29 12:32:06 +0100

    Promjena imena varijabli

--- src/index.ts
+++ src/index.ts
@@ -42,74 +42,80 @@ app.get('/',  function (req, res) {
 });
 
 app.post('/updateTable', async (req, res) => {
+    //first parameter is result, second is matchId and third is idOfCompetition
     const data = req.body.option.split(",");
     await pool.query(`UPDATE pairs SET result = ${data[0]} WHERE matchid = ${data[1]} AND idofcompetition = ${data[2]}`);
-    let nesto = "/updateCompetition?param="+data[2]
-    res.redirect(nesto)
+    let linkOfCompetition = "/updateCompetition?param="+data[2]
+    res.redirect(linkOfCompetition)
 })
 
+//!!!!!!!!!!!!!!
 app.get("/competition", async function (req, res) {
-  let newData = []
+  let newDataSorted = []
   pool.connect()
   try {
     const competitors = await pool.query('SELECT * FROM competitors');
-    const table = await pool.query(`SELECT * FROM pairs`);
+    const pairsTable = await pool.query(`SELECT * FROM pairs`);
     var competitions = await pool.query(`SELECT * FROM competitions`);
-    let newArray = []
-    let pomArray = []
+
+    let helpArray1 = []
+    let helpArray2 = []
+
     let index = competitors.rows[0].fk_id
     let fk_ids = new Set()
+
+    //spliting competitors by different ids of competition
     for (let i = 0; i < competitors.rows.length; ++i) {
       let newIndex = competitors.rows[i].fk_id
       fk_ids.add(newIndex);
       if (index != newIndex) {
-        newArray.push(pomArray)
-        pomArray = []
+        helpArray1.push(helpArray2)
+        helpArray2 = []
         index = newIndex
-        pomArray.push(competitors.rows[i])
+        helpArray2.push(competitors.rows[i])
       } else {
-        pomArray.push(competitors.rows[i])
-        if (i == competitors.rows.length - 1) newArray.push(pomArray)
+        helpArray2.push(competitors.rows[i])
+        if (i == competitors.rows.length - 1) helpArray1.push(helpArray2)
       }
     }
 
-    let proba = []
+    helpArray1 = []
     let competitorsFromOneTable = []
-    let test = []
+    let round = [] // all rounds of each competition
 
     const values = fk_ids.values();
     for (let k = 0; k < fk_ids.size; ++k) {
       const obj = values.next()
       competitorsFromOneTable = competitors.rows.filter(items => items.fk_id === obj.value);
-      test = table.rows.filter(items => items.idofcompetition === obj.value);
-      for (let i = 0; i < test.length; ++i) {
-        if (test[i].result == 1) {
-          competitorsFromOneTable[test[i].pair1 - 1].points = competitorsFromOneTable[test[i].pair1 - 1].points + 3;
-          competitorsFromOneTable[test[i].pair1 - 1].victory = competitorsFromOneTable[test[i].pair1 - 1].victory + 1;
-          competitorsFromOneTable[test[i].pair2 - 1].losses = competitorsFromOneTable[test[i].pair2 - 1].losses + 1;
+      round = pairsTable.rows.filter(items => items.idofcompetition === obj.value);
+      for (let i = 0; i < round.length; ++i) {
+        if (round[i].result == 1) {
+          competitorsFromOneTable[round[i].pair1 - 1].points = competitorsFromOneTable[round[i].pair1 - 1].points + 3;
+          competitorsFromOneTable[round[i].pair1 - 1].victory = competitorsFromOneTable[round[i].pair1 - 1].victory + 1;
+          competitorsFromOneTable[round[i].pair2 - 1].losses = competitorsFromOneTable[round[i].pair2 - 1].losses + 1;
 
-        } else if (test[i].result == 2) {
-          competitorsFromOneTable[test[i].pair1 - 1].points = competitorsFromOneTable[test[i].pair1 - 1].points + 1;
-          competitorsFromOneTable[test[i].pair2 - 1].points = competitorsFromOneTable[test[i].pair2 - 1].points + 1;
-          competitorsFromOneTable[test[i].pair1 - 1].tied = competitorsFromOneTable[test[i].pair1 - 1].tied + 1;
-          competitorsFromOneTable[test[i].pair2 - 1].tied = competitorsFromOneTable[test[i].pair2 - 1].tied + 1;
+        } else if (round[i].result == 2) {
+          competitorsFromOneTable[round[i].pair1 - 1].points = competitorsFromOneTable[round[i].pair1 - 1].points + 1;
+          competitorsFromOneTable[round[i].pair2 - 1].points = competitorsFromOneTable[round[i].pair2 - 1].points + 1;
+          competitorsFromOneTable[round[i].pair1 - 1].tied = competitorsFromOneTable[round[i].pair1 - 1].tied + 1;
+          competitorsFromOneTable[round[i].pair2 - 1].tied = competitorsFromOneTable[round[i].pair2 - 1].tied + 1;
 
-        } else if (test[i].result == 3) {
-          competitorsFromOneTable[test[i].pair2 - 1].points = competitorsFromOneTable[test[i].pair2 - 1].points + 3;
-          competitorsFromOneTable[test[i].pair1 - 1].losses = competitorsFromOneTable[test[i].pair1 - 1].losses + 1;
-          competitorsFromOneTable[test[i].pair2 - 1].victory = competitorsFromOneTable[test[i].pair2 - 1].victory + 1;
+        } else if (round[i].result == 3) {
+          competitorsFromOneTable[round[i].pair2 - 1].points = competitorsFromOneTable[round[i].pair2 - 1].points + 3;
+          competitorsFromOneTable[round[i].pair1 - 1].losses = competitorsFromOneTable[round[i].pair1 - 1].losses + 1;
+          competitorsFromOneTable[round[i].pair2 - 1].victory = competitorsFromOneTable[round[i].pair2 - 1].victory + 1;
         }
        }
-      proba.push(competitorsFromOneTable)
+       helpArray1.push(competitorsFromOneTable)
     }
-    for (let i = 0; i < proba.length; ++i) {
-      newData.push(proba[i].sort((a, b) => parseFloat(b.points) - parseFloat(a.points)))
+    for (let i = 0; i < helpArray1.length; ++i) {
+      newDataSorted.push(helpArray1[i].sort((a, b) => parseFloat(b.points) - parseFloat(a.points)))
     }
   } catch(err) {
     console.log(err)
   }
 
-  let authID = []
+  let authID = [] // ids of all competition that current logged user has created
   const ids = await pool.query(`SELECT * FROM users WHERE email = '${req.oidc.user?.email}'`);
   if (ids === undefined) {
     authID.push(0)
@@ -119,21 +125,21 @@ app.get("/competition", async function (req, res) {
     }
   }
 
-  let map = new Map()
-  let pomSet = [];
-  for (let i = 0; i < newData.length; ++i) {
-    for (let j = 0; j < newData[i].length; ++j) {
-      pomSet.push(newData[i][j])
+  let map = new Map() // map with keys and values -> row from competition table and belonging competitiors and their points
+  let helpArray1 = [];
+  for (let i = 0; i < newDataSorted.length; ++i) {
+    for (let j = 0; j < newDataSorted[i].length; ++j) {
+      helpArray1.push(newDataSorted[i][j])
     }
   }
-  for (let i = 0; i < newData.length; ++i) {
-    let idOfCompetition = newData[i][0].fk_id
+  for (let i = 0; i < newDataSorted.length; ++i) {
+    let idOfCompetition = newDataSorted[i][0].fk_id
     let competition = competitions.rows.find(item => item.id === idOfCompetition)
-    let oneData = pomSet.filter(items => items.fk_id === idOfCompetition);
+    let oneData = helpArray1.filter(items => items.fk_id === idOfCompetition);
     map.set(competition, oneData)
   }
   if (req.oidc.isAuthenticated()) {
-    res.render("competition.pug", {newData, authID, competitions, map})
+    res.render("competition.pug", {newDataSorted, authID, competitions, map})
   } else {
     res.redirect(302, '/')
   }
@@ -175,29 +181,29 @@ app.get("/updateCompetition", async function(req, res) {
     console.log(err)
   }
 
-  let test = pairs[0]
+  let round = pairs[0]
   let competitorsFromOneTable = data[0]
 
-  for (let i = 0; i < test.length; ++i) {
-    if (test[i].result == 1) {
-      competitorsFromOneTable[test[i].pair1 - 1].points = competitorsFromOneTable[test[i].pair1 - 1].points + 3;
-      competitorsFromOneTable[test[i].pair1 - 1].victory = competitorsFromOneTable[test[i].pair1 - 1].victory + 1;
-      competitorsFromOneTable[test[i].pair2 - 1].losses = competitorsFromOneTable[test[i].pair2 - 1].losses + 1;
+  for (let i = 0; i < round.length; ++i) {
+    if (round[i].result == 1) {
+      competitorsFromOneTable[round[i].pair1 - 1].points = competitorsFromOneTable[round[i].pair1 - 1].points + 3;
+      competitorsFromOneTable[round[i].pair1 - 1].victory = competitorsFromOneTable[round[i].pair1 - 1].victory + 1;
+      competitorsFromOneTable[round[i].pair2 - 1].losses = competitorsFromOneTable[round[i].pair2 - 1].losses + 1;
 
-    } else if (test[i].result == 2) {
-      competitorsFromOneTable[test[i].pair1 - 1].points = competitorsFromOneTable[test[i].pair1 - 1].points + 1;
-      competitorsFromOneTable[test[i].pair2 - 1].points = competitorsFromOneTable[test[i].pair2 - 1].points + 1;
-      competitorsFromOneTable[test[i].pair1 - 1].tied = competitorsFromOneTable[test[i].pair1 - 1].tied + 1;
-      competitorsFromOneTable[test[i].pair2 - 1].tied = competitorsFromOneTable[test[i].pair2 - 1].tied + 1;
+    } else if (round[i].result == 2) {
+      competitorsFromOneTable[round[i].pair1 - 1].points = competitorsFromOneTable[round[i].pair1 - 1].points + 1;
+      competitorsFromOneTable[round[i].pair2 - 1].points = competitorsFromOneTable[round[i].pair2 - 1].points + 1;
+      competitorsFromOneTable[round[i].pair1 - 1].tied = competitorsFromOneTable[round[i].pair1 - 1].tied + 1;
+      competitorsFromOneTable[round[i].pair2 - 1].tied = competitorsFromOneTable[round[i].pair2 - 1].tied + 1;
 
-    } else if (test[i].result == 3) {
-      competitorsFromOneTable[test[i].pair2 - 1].points = competitorsFromOneTable[test[i].pair2 - 1].points + 3;
-      competitorsFromOneTable[test[i].pair1 - 1].losses = competitorsFromOneTable[test[i].pair1 - 1].losses + 1;
-      competitorsFromOneTable[test[i].pair2 - 1].victory = competitorsFromOneTable[test[i].pair2 - 1].victory + 1;
+    } else if (round[i].result == 3) {
+      competitorsFromOneTable[round[i].pair2 - 1].points = competitorsFromOneTable[round[i].pair2 - 1].points + 3;
+      competitorsFromOneTable[round[i].pair1 - 1].losses = competitorsFromOneTable[round[i].pair1 - 1].losses + 1;
+      competitorsFromOneTable[round[i].pair2 - 1].victory = competitorsFromOneTable[round[i].pair2 - 1].victory + 1;
     }
   }
 
-  var onlyOnePair = test
+  var onlyOnePair = round
   onlyOnePair.sort((a, b) => parseInt(a.matchid) - parseInt(b.matchid));
 
   var newData = competitorsFromOneTable.sort((a, b) => parseFloat(b.points) - parseFloat(a.points))
@@ -227,6 +233,7 @@ app.post('/makeCompetition', async (req, res) => {
     var allCompetitors = competitors.split(';')
     if (allCompetitors.length < 4 || allCompetitors.length > 8) throw new Error("Možete unijeti samo od 4 do 8 natjecatelja")
 
+    //beginning of transaction
     await pool.query('BEGIN');
     try {
       await pool.query(`INSERT INTO "competitions" (name, typeofcompetition) VALUES ('${nameOfCompetition}', ${typeOfCompetition});`);
@@ -247,6 +254,7 @@ app.post('/makeCompetition', async (req, res) => {
          await pool.query(`INSERT INTO pairs (idofcompetition, pair1, pair2, result) VALUES (${newlyAddedId}, ${keys[j][k]}, ${keys[j][keys[j].length / 2 + k]}, 0)`);
       }
     }
+    //end of transaction
     await pool.query('COMMIT');
     res.redirect(302, '/competition');
   } catch (err) {
